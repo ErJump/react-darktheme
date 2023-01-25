506452b51ef263d712adcaaa191c086a3c0e0f13
ErJump <fedelijunior@gmail.com>
2023-01-25 09:36:00 +0100

    feat: :sparkles: Implements article + functions to app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import {useState, useEffect} from 'react';
+import data from './data';
+import Article from './components/Article';
+
+const getThemeFromLS = () => {
+  if (localStorage.getItem("theme")) {
+    return localStorage.getItem("theme");
+  } 
+  return "dark-theme"
+}
 
 function App() {
+  const [theme, setTheme] = useState(getThemeFromLS());
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme])
+
+
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <section className="section-center">
+      <div className="container">
+        <button className="btn">
+          {theme === "dark-theme" ? 'dark' : 'light'}
+        </button>
+        <section className="article-section">
+          {data.map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+        </section>
+      </div>
+    </section>
   );
 }
 
